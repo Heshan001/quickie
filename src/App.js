2ba1c8b1d3c55ad8cdea3a5f1d8d2c40a4ded614
Heshan001 <supunabayarathnabs@gmail.com>
2023-08-07 14:10:18 +0530

    add project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Login from './routes/login'
+
+function App() {
+  return (
+    <div>
+      
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/routes/select.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../styles/select.css'
+
+function Select() {
+  return (
+   <div> 
+    <div className='heading'>
+         Select You'r Student or University         
+    </div>
+    <div className='buttons'>
+        <button className='btn1'>Student</button>
+        <button className='btn2'>University</button>
+    </div>
+
+    </div>
+
+  )
+}
+
+export default Select
