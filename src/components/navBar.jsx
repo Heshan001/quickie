f67bd35a63008e16130447edc7e78d8510eac724
NipunS12 <114478939+NipunS12@users.noreply.github.com>
2023-08-21 15:44:40 +0530

    selection page update
    update styles

---
+++ src/components/navBar.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import '../styles/navBar.css'
+
+function NavBar() {
+
+    const navItems =[
+        {name : 'Home',
+         link: '/home'
+        },
+ 
+        {name : 'About',
+        link: '/about'
+        },
+ 
+        {name : 'Event',
+        link: '/event'
+        },
+ 
+        {name : 'Login',
+        link: '/login'
+        },
+ 
+        {name : 'Contact',
+        link: '/contact'
+        }
+     ]
+  return (
+    <div>
+        <nav className='navItems'>
+            
+
+            <ul className='navMenu'>
+            {navItems.map((item,index) => {
+                return(
+                    <li key = {index}>
+                        <a href={item.link}>{item.name}</a>
+                    </li>
+                )
+            })}
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default NavBar
